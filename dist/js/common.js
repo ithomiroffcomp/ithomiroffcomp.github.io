42ab4ae96e10d3ee18339370c77c999a19b63f06
ithomiroffcomp <the.victor@bk.ru>
2017-07-11 03:14:32 +0300

    last

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,24 +1,5 @@
 $(document).ready(function(){
   
-  //E-mail Ajax Send
-  $("#form").submit(function() { //Change
-    var th = $(this);
-    $.ajax({
-      type: "POST",
-      url: "mail.php", //Change
-      data: th.serialize()
-    }).done(function() {
-      alert("Спасибо за заявку!");
-      setTimeout(function() {
-        // Done Functions
-        th.trigger("reset");
-      }, 1000);
-    });
-    return false;
-  });
-
-
-
   var owl = $('.slider-1');
       owl.owlCarousel({
         margin: 10,
